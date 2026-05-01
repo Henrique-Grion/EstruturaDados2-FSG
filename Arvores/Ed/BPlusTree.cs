c80883f8fb1ce69bf4732b6023110d570bf70b3a
Henrique-Grion <henriquegrion@hotmail.com>
2026-05-01 08:15:35 -0300

    as arvere

---
+++ Arvores/Ed/BPlusTree.cs
@@ -0,0 +1,212 @@
+﻿namespace Arvores.Ed;
+
+public class BPlusTree<TKey, TValue> where TKey : IComparable<TKey>
+{
+    private abstract class Node
+    {
+        public List<TKey> Keys = new List<TKey>();
+        public abstract bool IsLeaf { get; }
+    }
+
+    private class InternalNode : Node
+    {
+        public List<Node> Children = new List<Node>();
+        public override bool IsLeaf => false;
+    }
+
+    private class LeafNode : Node
+    {
+        public List<TValue> Values = new List<TValue>();
+        public LeafNode Next;
+        public override bool IsLeaf => true;
+    }
+
+    private readonly int _t; // grau mínimo
+    private Node _root;
+
+    public BPlusTree(int t)
+    {
+        if (t < 2)
+            throw new ArgumentException("t deve ser >= 2");
+
+        _t = t;
+        _root = new LeafNode();
+    }
+
+    // ---------------------------------------------------------
+    // BUSCA
+    // ---------------------------------------------------------
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        var leaf = FindLeaf(_root, key);
+
+        for (int i = 0; i < leaf.Keys.Count; i++)
+        {
+            if (leaf.Keys[i].CompareTo(key) == 0)
+            {
+                value = leaf.Values[i];
+                return true;
+            }
+        }
+
+        value = default!;
+        return false;
+    }
+
+    private LeafNode FindLeaf(Node node, TKey key)
+    {
+        if (node.IsLeaf)
+            return (LeafNode)node;
+
+        var internalNode = (InternalNode)node;
+
+        int i = 0;
+        while (i < internalNode.Keys.Count && key.CompareTo(internalNode.Keys[i]) >= 0)
+            i++;
+
+        return FindLeaf(internalNode.Children[i], key);
+    }
+
+    // ---------------------------------------------------------
+    // INSERÇÃO
+    // ---------------------------------------------------------
+    public void Insert(TKey key, TValue value)
+    {
+        var root = _root;
+
+        if (root.Keys.Count == 2 * _t - 1)
+        {
+            var newRoot = new InternalNode();
+            newRoot.Children.Add(root);
+            SplitChild(newRoot, 0);
+            _root = newRoot;
+        }
+
+        InsertNonFull(_root, key, value);
+    }
+
+    private void InsertNonFull(Node node, TKey key, TValue value)
+    {
+        if (node.IsLeaf)
+        {
+            var leaf = (LeafNode)node;
+
+            int i = leaf.Keys.Count - 1;
+            leaf.Keys.Add(default!);
+            leaf.Values.Add(default!);
+
+            while (i >= 0 && key.CompareTo(leaf.Keys[i]) < 0)
+            {
+                leaf.Keys[i + 1] = leaf.Keys[i];
+                leaf.Values[i + 1] = leaf.Values[i];
+                i--;
+            }
+
+            leaf.Keys[i + 1] = key;
+            leaf.Values[i + 1] = value;
+        }
+        else
+        {
+            var internalNode = (InternalNode)node;
+
+            int i = internalNode.Keys.Count - 1;
+            while (i >= 0 && key.CompareTo(internalNode.Keys[i]) < 0)
+                i--;
+
+            i++;
+
+            if (internalNode.Children[i].Keys.Count == 2 * _t - 1)
+            {
+                SplitChild(internalNode, i);
+
+                if (key.CompareTo(internalNode.Keys[i]) >= 0)
+                    i++;
+            }
+
+            InsertNonFull(internalNode.Children[i], key, value);
+        }
+    }
+
+    // ---------------------------------------------------------
+    // SPLIT
+    // ---------------------------------------------------------
+    private void SplitChild(InternalNode parent, int index)
+    {
+        Node full = parent.Children[index];
+
+        if (full.IsLeaf)
+            SplitLeaf(parent, index, (LeafNode)full);
+        else
+            SplitInternal(parent, index, (InternalNode)full);
+    }
+
+    private void SplitLeaf(InternalNode parent, int index, LeafNode full)
+    {
+        var newLeaf = new LeafNode();
+
+        int mid = _t;
+
+        // mover metade direita
+        for (int i = mid; i < full.Keys.Count; i++)
+        {
+            newLeaf.Keys.Add(full.Keys[i]);
+            newLeaf.Values.Add(full.Values[i]);
+        }
+
+        full.Keys.RemoveRange(mid, full.Keys.Count - mid);
+        full.Values.RemoveRange(mid, full.Values.Count - mid);
+
+        // encadear folhas
+        newLeaf.Next = full.Next;
+        full.Next = newLeaf;
+
+        // chave promovida (primeira da nova folha)
+        TKey promoted = newLeaf.Keys[0];
+
+        parent.Keys.Insert(index, promoted);
+        parent.Children.Insert(index + 1, newLeaf);
+    }
+
+    private void SplitInternal(InternalNode parent, int index, InternalNode full)
+    {
+        var newNode = new InternalNode();
+
+        int mid = _t - 1;
+
+        TKey promoted = full.Keys[mid];
+
+        // mover metade direita
+        for (int i = mid + 1; i < full.Keys.Count; i++)
+            newNode.Keys.Add(full.Keys[i]);
+
+        for (int i = mid + 1; i < full.Children.Count; i++)
+            newNode.Children.Add(full.Children[i]);
+
+        full.Keys.RemoveRange(mid, full.Keys.Count - mid);
+        full.Children.RemoveRange(mid + 1, full.Children.Count - (mid + 1));
+
+        parent.Keys.Insert(index, promoted);
+        parent.Children.Insert(index + 1, newNode);
+    }
+
+    // ---------------------------------------------------------
+    // SCAN (range)
+    // ---------------------------------------------------------
+    public IEnumerable<(TKey Key, TValue Value)> Scan()
+    {
+        var node = _root;
+
+        while (!node.IsLeaf)
+            node = ((InternalNode)node).Children[0];
+
+        var leaf = (LeafNode)node;
+
+        while (leaf != null)
+        {
+            for (int i = 0; i < leaf.Keys.Count; i++)
+                yield return (leaf.Keys[i], leaf.Values[i]);
+
+            leaf = leaf.Next;
+        }
+    }
+}
---
+++ Arvores/Ed/RedBlackTree.cs
@@ -0,0 +1,264 @@
+﻿
+namespace Arvores.Ed;
+
+public enum NodeColor
+{
+    Red,
+    Black
+}
+
+public class RedBlackTree<TKey, TValue> where TKey : IComparable<TKey>
+{
+    private class Node(TKey key, TValue value, NodeColor color)
+    {
+        public TKey Key { get; set; } = key;
+        public TValue Value { get; set; } = value;
+        public NodeColor Color { get; set; } = color;
+        public Node? Left = null;
+        public Node? Right = null;
+        public Node? Parent = null;
+
+        public bool IsRed => Color == NodeColor.Red;
+        public bool IsBlack => Color == NodeColor.Black;
+    }
+
+    private Node? _root = null;
+    private readonly IComparer<TKey> _comparer;
+
+    public RedBlackTree() : this(Comparer<TKey>.Default) { }
+
+    public RedBlackTree(IComparer<TKey> comparer)
+    {
+        _comparer = comparer ?? Comparer<TKey>.Default;
+    }
+
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        var node = FindNode(key);
+        if (node != null)
+        {
+            value = node.Value;
+            return true;
+        }
+
+        value = default!;
+        return false;
+    }
+
+    public void Insert(TKey key, TValue value)
+    {
+        var newNode = new Node(key, value, NodeColor.Red);
+        InsertBST(newNode);
+        FixInsert(newNode);
+    }
+
+    public bool ContainsKey(TKey key) => FindNode(key) != null;
+
+    private Node? FindNode(TKey key)
+    {
+        var current = _root;
+        while (current != null)
+        {
+            int cmp = _comparer.Compare(key, current.Key);
+            if (cmp == 0)
+                return current;
+            current = cmp < 0 ? current.Left : current.Right;
+        }
+        return null;
+    }
+
+    private void InsertBST(Node node)
+    {
+        if (_root == null)
+        {
+            _root = node;
+            return;
+        }
+
+        Node? current = _root;
+        Node? parent = null;
+
+        while (current != null)
+        {
+            parent = current;
+            int cmp = _comparer.Compare(node.Key, current.Key);
+            current = cmp < 0 ? current.Left : current.Right;
+        }
+
+        if (parent != null)
+        {
+            node.Parent = parent;
+            if (_comparer.Compare(node.Key, parent.Key) < 0)
+                parent.Left = node;
+            else
+                parent.Right = node;
+        }
+    }
+
+    private void RotateLeft(Node x)
+    {
+        Node? y = x.Right;
+        x.Right = y?.Left;
+        y?.Left?.Parent = x;
+
+        y?.Parent = x.Parent;
+        if (x.Parent == null)
+            _root = y;
+        else if (x == x.Parent.Left)
+            x.Parent.Left = y;
+        else
+            x.Parent.Right = y;
+
+        y?.Left = x;
+        x.Parent = y;
+    }
+
+    private void RotateRight(Node y)
+    {
+        Node? x = y.Left;
+        y.Left = x?.Right;
+        if (x?.Right != null)
+            x.Right.Parent = y;
+
+        x?.Parent = y.Parent;
+        if (y.Parent == null)
+            _root = x;
+        else if (y == y.Parent.Left)
+            y.Parent.Left = x;
+        else
+            y.Parent.Right = x;
+
+        x?.Right = y;
+        y.Parent = x;
+    }
+
+    private void FixInsert(Node z)
+    {
+        while (z?.Parent != null && z.Parent.IsRed)
+        {
+            if (z.Parent == z.Parent.Parent?.Left)
+            {
+                Node? y = z.Parent.Parent.Right;
+                if (y != null && y.IsRed)
+                {
+                    z.Parent.Color = NodeColor.Black;
+                    y.Color = NodeColor.Black;
+                    z.Parent.Parent.Color = NodeColor.Red;
+                    z = z.Parent.Parent;
+                }
+                else
+                {
+                    if (z == z.Parent.Right)
+                    {
+                        z = z.Parent;
+                        RotateLeft(z);
+                    }
+                    z?.Parent?.Color = NodeColor.Black;
+                    z?.Parent?.Parent?.Color = NodeColor.Red;
+                    if (z?.Parent?.Parent != null)
+                        RotateRight(z.Parent.Parent);
+                }
+            }
+            else
+            {
+                // simétrico
+                Node y = z.Parent.Parent.Left; // tio
+                if (y != null && y.IsRed)
+                {
+                    z.Parent.Color = NodeColor.Black;
+                    y.Color = NodeColor.Black;
+                    z.Parent.Parent.Color = NodeColor.Red;
+                    z = z.Parent.Parent;
+                }
+                else
+                {
+                    if (z == z.Parent.Left)
+                    {
+                        z = z.Parent;
+                        RotateRight(z);
+                    }
+                    z.Parent.Color = NodeColor.Black;
+                    z.Parent.Parent.Color = NodeColor.Red;
+                    RotateLeft(z.Parent.Parent);
+                }
+            }
+        }
+
+        _root.Color = NodeColor.Black;
+    }
+
+    public IEnumerable<KeyValuePair<TKey, TValue>> InOrder()
+    {
+        if (_root == null) yield break;
+
+        var stack = new Stack<Node>();
+        var current = _root;
+
+        while (stack.Count > 0 || current != null)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.Left;
+            }
+
+            current = stack.Pop();
+            yield return new KeyValuePair<TKey, TValue>(current.Key, current.Value);
+            current = current.Right;
+        }
+    }
+
+    public bool Remove(TKey key)
+    {
+        var node = FindNode(key);
+        if (node == null)
+            return false;
+
+        DeleteNode(node);
+        return true;
+    }
+
+    private void Transplant(Node u, Node v)
+    {
+        if (u.Parent == null)
+            _root = v;
+        else if (u == u.Parent.Left)
+            u.Parent.Left = v;
+        else
+            u.Parent.Right = v;
+
+        if (v != null)
+            v.Parent = u.Parent;
+    }
+
+    private Node Minimum(Node node)
+    {
+        while (node.Left != null)
+            node = node.Left;
+        return node;
+    }
+    private void DeleteNode(Node z)
+    {
+        if (z.Left == null)
+        {
+            Transplant(z, z.Right);
+        }
+        else if (z.Right == null)
+        {
+            Transplant(z, z.Left);
+        }
+        else
+        {
+            Node y = Minimum(z.Right);
+            if (y.Parent != z)
+            {
+                Transplant(y, y.Right);
+                y.Right = z.Right;
+                y.Right.Parent = y;
+            }
+            Transplant(z, y);
+            y.Left = z.Left;
+            y.Left.Parent = y;
+        }
+    }
+}
